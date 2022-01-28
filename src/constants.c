b414d8efa7a02c6362616c860a7f05c17f91c864
elzaidir <mathieu@bourquenoud.net>
2022-01-29 00:19:12 +0100

    All constant type are checked against duplicates

--- src/constants.c
+++ src/constants.c
@@ -1,26 +1,25 @@
 #include "constants.h"
 
+#include "failure.h"
+
 hash_t* bit_const_array;
 hash_t* int_const_array;
 hash_t* str_const_array;
 
-bool check_any(char* key)
+void check_any(char* key)
 {
-    return (
-        hash_check_key(bit_const_array, key ) |
+    if ( hash_check_key(bit_const_array, key ) |
         hash_check_key(int_const_array, key ) |
-        hash_check_key(str_const_array, key )
-    );
+        hash_check_key(str_const_array, key ))
+        {
+            fail("'%s' redeclared. You can not declare a constant twice.\n", key);
+        }
 }
 
 int command_bit_const(data_t *id, data_t *value)
 {
     //Check if the constant already exists
-    if (check_any(id->strVal))
-    {
-        printf("'%s' redeclared. You can not declare a constant twice.\n", id->strVal);
-        return -1;
-    }
+    check_any(id->strVal);
 
     bit_const_t* data = &(value->bVal);
 
@@ -32,11 +31,7 @@ int command_bit_const(data_t *id, data_t *value)
 int command_int_const(data_t *id, data_t *value)
 {
     //Check if the constant already exists
-    if (check_any(id->strVal))
-    {
-        printf("'%s' redeclared. You can not declare a constant twice.\n", id->strVal);
-        return -1;
-    }
+    check_any(id->strVal);
 
     hash_add(int_const_array, id->strVal, (void*)value);
     return 0; //Success
@@ -45,11 +40,7 @@ int command_int_const(data_t *id, data_t *value)
 int command_str_const(data_t *id, data_t *value)
 {
     //Check if the constant already exists
-    if (check_any(id->strVal))
-    {
-        printf("'%s' redeclared. You can not declare a constant twice.\n", id->strVal);
-        return -1;
-    }
+    check_any(id->strVal);
 
     hash_add(str_const_array, id->strVal, (void*)value);
     return 0; //Success
