39377c3a84e02d00154b49dd19bfb806b2756aa3
elzaidir <mathieu@bourquenoud.net>
2022-02-11 21:04:52 +0100

    Print state machine in dot format

--- src/state_machine.c
+++ src/state_machine.c
@@ -35,11 +35,40 @@ void _state_add_transition(int _arg_count, state_t *state, int next_state_id, ..
     for (int i = 0; i < _arg_count; i++)
     {
         int condition = va_arg(args, int);
-        darray_add(new_transition.conditions_tint, condition);
+void state_machine_print(state_machine_t *state_machine, FILE *file_descriptor)
+{
+    // Get the dynamic array as a static array
+    state_t *state_array = (state_t *)(state_machine->states_tstate->element_list);
+
+    fputs("digraph STATE_MACHINE {\n"
+          "\tnode [shape=ellipse,width=1.25]\n"
+          "\tmclimit=100\n"
+          "\tsplines=spline\n\n",
+          file_descriptor);
+
+    // Print all state and transitions
+    for (int i = 0; i < state_machine->states_tstate->count; i++)
+    {
+        // Get the dynamic array as a static array
+        transistion_t *transition = (transistion_t *)(state_array[i].transitions_ttrans->element_list);
+
+        for (int j = 0; j < state_array[i].transitions_ttrans->count; j++)
+        {
+            fprintf(file_descriptor, "\t%i -> %i", state_array[i].id, transition[j].next_state_id);
+            fputs("  \t[label=\"(", file_descriptor);
+            for (size_t k = 0; k < transition[j].conditions_tint->count; k++)
+            {
+                fprintf(
+                    file_descriptor,
+                    "%i%c",
+                    *((int *)(transition[j].conditions_tint->element_list) + k),
+                    k == transition[j].conditions_tint->count - 1 ? ')' : ',');
+            }
+            fputs("\"]\n", file_descriptor);
+        }
     }
 
-    //Add the new transition the the state
-    darray_add(state->transitions_ttrans, new_transition);
+    fputs("}\n", file_descriptor);
 }
 
 /********************************************************/
--- src/state_machine.h
+++ src/state_machine.h
@@ -1,23 +1,29 @@
 #pragma once
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+#include "macro.h"
+#include "xmalloc.h"
 #include "dynamic_array.h"
+#include "hash_array.h"
 
 typedef struct state_s state_t;
 
 typedef struct
 {
     int next_state_id;
-    darray_t* conditions_tint;
-}transistion_t;
+    darray_t *conditions_tint;
+} transistion_t;
 
 struct state_s
 {
     int id;
-    darray_t* transitions_ttrans;
+    int output;
+    darray_t *transitions_ttrans;
 };
 
 state_t state_init_state(int id);
-void state_add_state(state_t *state_list, int id);
-
-void _state_add_transition(int _arg_count, state_t *state, int next_state_id, ...);
-#define state_add_transition(...) _state_add_transition((PP_NARG(__VA_ARGS__)) - 1, __VA_ARGS__)
+void state_machine_print(state_machine_t *state_machine, FILE *file_descriptor);
