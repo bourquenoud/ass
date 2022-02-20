5f58690754f1906f92f522cebd19d5e2fbec3a86
elzaidir <mathieu@bourquenoud.net>
2022-02-20 17:32:12 +0100

    Used the pattern_compiler for the parser generator Generating the parser state machine now makes use of the pattern_compiler to allow more flexibility

--- src/parser.c
+++ src/parser.c
@@ -23,42 +23,5 @@ state_machine_t parser_arrays_to_nfa(int count, const rule_def_t **rules)
 
 state_machine_t parser_rule_to_nfa(const rule_def_t *rule)
 {
-    if (rule->count == 0)
-    {
-        fail_error("Empty rule");
-        exit(EXIT_FAILURE);
-    }
-
-    state_machine_t new_state_machine = state_machine_init();
-
-    state_t *current_state = darray_get_ptr(&(new_state_machine.states_tstate), 0); // State to be processed
-    state_t *last_state = NULL;                                                     // Last processed state
-    state_t *end_state = NULL;                                                      // State pointed by this pointer at the end is marked as final state
-
-    state_t *tmp_state = NULL;
-
-    // The set of all conditions
-    darray_t *set = darray_init(sizeof(char));
-
-    bool escaped = false;
-    bool optional = false;
-
-    // Used for the set parsing
-    bool parsing_set = false;  // If we are processing a set
-    bool negative_set = false; // If the set we are processing is negative
-
-    char processed_char;
-
-    for (size_t i = 0; i < rule->count; i++)
-    {
-        last_state = current_state;
-
-        // Generate a new state and add all the characters in the set
-        current_state = state_machine_add_state(&new_state_machine, false);
-        state_add_transition(last_state, current_state->id, rule->tokens[i]);
-    }
-    current_state->end_state = true;
-    current_state->output = rule->id;
-
-    return new_state_machine;
+    return pattern_compiler(rule->count, rule->tokens, rule->id);
 }
--- src/parser.h
+++ src/parser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "state_machine.h"
+#include "pattern_compiler.h"
 
 typedef struct
 {
