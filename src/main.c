282d85d6244ca8d35b7d64c07a50acf475e225f9
elzaidir <mathieu@bourquenoud.net>
2022-03-02 17:12:30 +0100

    Added option parsing and file checking

--- src/main.c
+++ src/main.c
@@ -23,14 +23,59 @@
 
 int main(int argc, char **argv)
 {
+    // File descriptor for the output file
+    // TODO: more meaningful naming
+    FILE *fd;
+    char *output_file = NULL;
     char *file_list[argc];
     int file_count = 0;
+    int opt;
 
-    for (int i = 1; i < argc; i++)
+    // Parse options
+    while ((opt = getopt(argc, argv, ":o:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'o': // Output file
+            printf("Setting output file as %s\n", optarg);
+            if (output_file != NULL)
+                printf("WARNING : Output file path overriden.\n");
+            output_file = optarg;
+            break;
+        case ':':
+            printf("ERROR : Option '%c' expects an argument\n", opt);
+            exit(EXIT_FAILURE);
+            break;
+        case '?':
+            printf("ERROR : Unknown option '%c'\n", optopt);
+            exit(EXIT_FAILURE);
+            break;
+        default:
+            abort();
+        }
+    }
+
+    // Non-option arguments (parsed as input files)
+    bool failed = false;
+    for (int i = optind; i < argc; i++)
     {
         file_list[file_count] = argv[i];
+        // Check if we can read it before
+        if (access(file_list[file_count], R_OK) != 0)
+        {
+            printf("ERROR : %s (%s)\n", strerror(errno), file_list[file_count]);
+            failed = true;
+        }
         file_count++;
     }
+
+    if (failed)
+        exit(EXIT_FAILURE);
+    
+    if (output_file == NULL)
+        fd = stdin;
+    else
+        fd = fopen(output_file, "w");
 
     // Gives repeatable values for the name generation
     srand(1);
@@ -54,7 +99,6 @@ int main(int argc, char **argv)
     parser_generate();
 
     // Generate the file
-    FILE *fd = fopen("parser.c", "w");
     generator_set_file_descriptor(fd);
     generate(fd);
     fclose(fd);
