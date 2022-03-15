b7908e818bd5f82514489cf9beba5e4e9befda8a
elzaidir <mathieu@bourquenoud.net>
2022-03-15 16:04:46 +0100

    Updated example with constants

--- examples/pic16f887.asm
+++ examples/pic16f887.asm
@@ -1,19 +1,25 @@
-0x0:    GOTO    main
+;Memory address declaration
+.constant       porta,  0x05
+.constant       tx_reg, 0x19
+.constant       status, 0x83
+.constant       var,    0x50 ;Variable address to store the current data
+
+0x0:    GOTO    main 
 
 0x10: ;;Comment
 main:
-_press: BTFSS   0x05,b0
-        GOTO    _press
-relea:  BTFSC   0x05,b0
+press: BTFSS   porta,b0
+        GOTO    press
+relea:  BTFSC   porta,b0
         GOTO    relea
 
 print:  MOVLW   ' '    ;Start from z
-        MOVWF   0x50
+        MOVWF   var
 
-loop:   DECF    0x50,F
-        MOVF    0x50,W
-        MOVWF   0x19
+loop:   DECF    var,F
+        MOVF    var,W
+        MOVWF   tx_reg
         SUBLW   'a'
-        BTFSS   0x83,b2
+        BTFSS   status,b2
         GOTO    loop
         GOTO    main
