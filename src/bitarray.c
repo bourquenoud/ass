7d26b2b681a0c28957e07eec716875d5c66f2ee0
elzaidir <mathieu@bourquenoud.net>
2022-03-31 14:54:52 +0200

    Added comments to bitarray

--- src/bitarray.c
+++ src/bitarray.c
@@ -1,5 +1,6 @@
 #include "bitarray.h"
 
+// Set the bit at index to val
 void bitarray_set(bitarray_t *bitarray, int index, bool val)
 {
     int uint32_index = index / 32;
@@ -10,12 +11,14 @@ void bitarray_set(bitarray_t *bitarray, int index, bool val)
         bitarray->data[uint32_index] &= ~mask;
 }
 
+// Set all bits to val
 void bitarray_set_all(bitarray_t *bitarray, bool val)
 {
     uint32_t memcontent = (val) ? 0xFFFFFFFF : 0x00000000;
     memset(bitarray->data, memcontent, sizeof(bitarray->data));
 }
 
+// Return the union of bitarray_a and bitarray_b
 bitarray_t bitarray_union(bitarray_t *bitarray_a, bitarray_t *bitarray_b)
 {
     bitarray_t new_bitarray;
@@ -27,6 +30,7 @@ bitarray_t bitarray_union(bitarray_t *bitarray_a, bitarray_t *bitarray_b)
     return new_bitarray;
 }
 
+// Return the intersection of bitarray_a and bitarray_b
 bitarray_t bitarray_intersection(bitarray_t *bitarray_a, bitarray_t *bitarray_b)
 {
     bitarray_t new_bitarray;
@@ -38,6 +42,7 @@ bitarray_t bitarray_intersection(bitarray_t *bitarray_a, bitarray_t *bitarray_b)
     return new_bitarray;
 }
 
+// Return true if bitarray_a and bitarray_b are equal
 bool bitarray_compare(bitarray_t *bitarray_a, bitarray_t *bitarray_b)
 {
     for (size_t i = 0; i < BITARRAY_WIDTH / 32; i++)
@@ -48,6 +53,7 @@ bool bitarray_compare(bitarray_t *bitarray_a, bitarray_t *bitarray_b)
     return true;
 }
 
+// Return the value of the bit at index
 bool bitarray_get(bitarray_t *bitarray, int index)
 {
     int uint32_index = index / 32;
--- src/bitarray.h
+++ src/bitarray.h
@@ -12,9 +12,49 @@ typedef struct
     uint32_t data[BITARRAY_WIDTH / sizeof(uint32_t)];
 } bitarray_t;
 
+/**
+ * @brief Set the bit at index to val
+ * @param bitarray The bitarray to set
+ * @param index The index of the bit to set
+ * @param val The value to set the bit to
+ */
 void bitarray_set(bitarray_t *bitarray, int index, bool val);
+
+/**
+ * @brief Set all bits to val
+ * @param bitarray The bitarray to set
+ * @param val The value to set the bits to
+ */
 void bitarray_set_all(bitarray_t *bitarray, bool val);
+
+/**
+ * @brief Return the union of bitarray_a and bitarray_b
+ * @param bitarray_a The first bitarray
+ * @param bitarray_b The second bitarray
+ * @return The union of bitarray_a and bitarray_b
+ */
 bitarray_t bitarray_union(bitarray_t *bitarray_a, bitarray_t *bitarray_b);
+
+/**
+ * @brief Return the intersection of bitarray_a and bitarray_b
+ * @param bitarray_a The first bitarray
+ * @param bitarray_b The second bitarray
+ * @return The intersection of bitarray_a and bitarray_b
+ */
 bitarray_t bitarray_intersection(bitarray_t *bitarray_a, bitarray_t *bitarray_b);
+
+/**
+ * @brief Return true if bitarray_a and bitarray_b are equal
+ * @param bitarray_a The first bitarray
+ * @param bitarray_b The second bitarray
+ * @return true if bitarray_a and bitarray_b are equal
+ */
 bool bitarray_compare(bitarray_t *bitarray_a, bitarray_t *bitarray_b);
+
+/**
+ * @brief Return the value of the bit at index
+ * @param bitarray The bitarray to get the bit from
+ * @param index The index of the bit to get
+ * @return The value of the bit at index
+ */
 bool bitarray_get(bitarray_t *bitarray, int index);
